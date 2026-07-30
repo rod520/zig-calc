26de1e1eba2b90d6484ff70850fdd650b33f50e7
rod520 <rodriguesorlen@gmail.com>
2026-07-30 17:16:38 -0500

    blinky

---
+++ src/main.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const microzig = @import("microzig");
+const rp2xxx = microzig.hal;
+const time = rp2xxx.time;
+
+pub const panic = microzig.panic;
+pub const std_options = microzig.std_options(.{});
+
+comptime {
+    _ = microzig.export_startup();
+}
+
+// Compile-time pin configuration
+const pin_config = rp2xxx.pins.GlobalConfiguration{
+    .GPIO25 = .{
+        .name = "led",
+        .direction = .out,
+    },
+};
+
+pub fn main() !void {
+    const pins = pin_config.apply();
+
+    while (true) {
+        pins.led.toggle();
+        time.sleep_ms(250);
+    }
+}
