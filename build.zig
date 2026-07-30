f522cef2f347d9b52e5c3877518785555d69ac91
rod520 <rodriguesorlen@gmail.com>
2026-07-30 18:32:54 -0500

    we're zig-calc, actually

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
     const mb = MicroBuild.init(b, mz_dep) orelse return;
 
     const firmware = mb.add_firmware(.{
-        .name = "blinky",
+        .name = "zig-calc",
         .target = mb.ports.rp2xxx.boards.raspberrypi.pico2_arm,
         .optimize = .ReleaseSmall,
         .root_source_file = b.path("src/main.zig"),
@@ -19,5 +19,4 @@ pub fn build(b: *std.Build) void {
     // We call this twice to demonstrate that the default binary output for
     // RP2040 is UF2, but we can also output other formats easily
     mb.install_firmware(firmware, .{ });
-    mb.install_firmware(firmware, .{ .format = .elf });
 }
